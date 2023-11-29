3b556c0f5e4dca58ab25f18af56b80a2bc8478b3
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2023-11-29 22:14:13 +0300

    add feature

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# React + Vite
+# React Data Visualizer
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Welcome to the Visualize Random Data App! This application showcases the visualization of randomly generated data using cutting-edge technologies.
 
-Currently, two official plugins are available:
+## How to Run
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+To run the project on your local machine, run the lines in the bash below:
+
+   ```bash
+   git clone https://github.com/hbedirhan/react-data-visualizer
+   cd react-data-visualizer
+   npm install
+   npm run dev
+
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -12,20 +12,27 @@ function Input({ setInterval, interval, numberRange, setNumberRange }) {
 
 
   return (
-    <>
+    <div className='input-group'>
+      <div className='inputs'>
       <input
         className={interval >= 1 ? 'input' : 'alert-input'}
+        id='interval'
         type="number"
         onChange={handleChangeInterval}
         placeholder='generation time interval (default: 1000ms)'
         min={1} />
+        {interval >= 1 ? '' : <label className='alert' htmlFor="interval">The generation time should be at least 1 for this.</label>}
+      </div>
+      <div className='inputs'>
       <input
         className={numberRange >= 1 ? 'input' : 'alert-input'}
         type="number"
         onChange={handleChangeRange}
         placeholder='maximum range (default: 10)'
         min={1} />
-    </>
+        {numberRange >= 1 ? '' : <label className='alert' htmlFor="interval">The range should be at least 1 for this.</label>}
+      </div>
+    </div>
   )
 }
 
